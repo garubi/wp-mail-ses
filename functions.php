c5884718c14b6552e44ec76205234d9befd74290
stefano <sgaruti@gmail.com>
2025-10-24 12:09:27 +0200

    usa il filtro pre_wp_mail() invece di fare override di wp_mail()

--- functions.php
+++ functions.php
@@ -1,18 +1,29 @@
 <?php
 
-if ( function_exists( 'wp_mail' ) ) {
-	add_action(
-		'admin_notices',
-		array( WP_Mail_SES::get_instance(), 'warning_wp_mail_exists' )
+// if ( function_exists( 'wp_mail' ) ) {
+// 	add_action(
+// 		'admin_notices',
+// 		array( WP_Mail_SES::get_instance(), 'warning_wp_mail_exists' )
+// 	);
+// } else {
+// 	function wp_mail( $to, $subject, $message, $headers = '', $attachments = '' ) {
+// 		return WP_Mail_SES::get_instance()->send_email(
+// 			$to,
+// 			$subject,
+// 			$message,
+// 			$headers,
+// 			$attachments
+// 		);
+// 	}
+// }
+
+add_filter( 'pre_wp_mail', 'wp_mail_ses', 10, 5 );
+function wp_mail_ses( $null, $to, $subject, $message, $headers = '', $attachments = '' ) {
+	return WP_Mail_SES::get_instance()->send_email(
+		$to,
+		$subject,
+		$message,
+		$headers,
+		$attachments
 	);
-} else {
-	function wp_mail( $to, $subject, $message, $headers = '', $attachments = '' ) {
-		return WP_Mail_SES::get_instance()->send_email(
-			$to,
-			$subject,
-			$message,
-			$headers,
-			$attachments
-		);
-	}
 }
